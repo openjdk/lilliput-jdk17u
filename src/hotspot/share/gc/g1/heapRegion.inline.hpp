590742c5bb0e979e191fa9cb320609de8b237916
Roman Kennke <rkennke@amazon.com>
2023-02-22 19:12:13 +0000

    Reformat asserts for better readability

--- src/hotspot/share/gc/g1/heapRegion.inline.hpp
+++ src/hotspot/share/gc/g1/heapRegion.inline.hpp
@@ -186,8 +186,13 @@ inline size_t HeapRegion::block_size(const HeapWord *addr) const {
   if (block_is_obj(addr)) {
     oop obj = cast_to_oop(addr);
 #ifdef _LP64
-    assert(!G1CollectedHeap::heap()->collector_state()->in_full_gc() || !RESOLVE, "don't resolve during full-GC");
-    assert(G1CollectedHeap::heap()->collector_state()->in_full_gc() || !obj->is_forwarded() || RESOLVE, "resolve object during non-full when forwarded");
+#ifdef ASSERT
+    if (RESOLVE) {
+      assert(!G1CollectedHeap::heap()->collector_state()->in_full_gc(), "Illegal/excessive resolve during full-GC");
+    } else {
+      assert(G1CollectedHeap::heap()->collector_state()->in_full_gc() || !obj->is_forwarded(), "Missing resolve when forwarded during normal GC");
+    }
+#endif
     if (RESOLVE && obj->is_forwarded()) {
       obj = obj->forwardee();
     }
